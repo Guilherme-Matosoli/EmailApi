be858b0940b4cca002057ac81da485abb1644cb1
Matosoli <guilhermematosgto2015@outlook.com>
2023-09-10 18:24:07 -0300

    feature: configure nodemailer :message:

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,5 +3,5 @@ import { Request, Response } from "express";
 export class SendMailController{
   async send( req: Request, res: Response ){
 
-  }
+  };
 }
---
+++ src/services/nodeMailer.ts
@@ -0,0 +1,13 @@
+import nodemailer from "nodemailer";
+
+const { EMAIL, PASSWORD } = process.env;
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth:{
+    user: "",
+    pass: ""
+  }
+});
